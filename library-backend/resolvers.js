48bfd937c70cfd932fbd288d00c9cd4d1b54664e
McMnyages <morachasilas@gmail.com>
2026-09-19 18:55:11 +0300

    refactor: improve resolvers structure and enhance editAuthor mutation

--- library-backend/resolvers.js
+++ library-backend/resolvers.js
@@ -1,41 +1,57 @@
-const { v1: uuid } = require('uuid')
 const Author = require('./models/author')
 const Book = require('./models/book')
 
 const resolvers = {
-  Query: {
-    bookCount: () => Book.collection.countDocuments(),
-    authorCount: async () => await Author.collection.countDocuments(),
-    allBooks: async (root, args) => {
-     return await Book.find({})
+    Query: {
+        bookCount: async () => Book.collection.countDocuments(),
+        authorCount: async () => Author.collection.countDocuments(),
+        allBooks: async (root, args) => {
+            let query = {}
+
+            if (args.author) {
+                const author = await Author.findOne({ name: args.author })
+                if (!author) {
+                    return []
+                }
+                query.author = author._id
+            }
+
+            if (args.genre) {
+                query.genres = args.genre
+            }
+            return Book.find(query).populate('author')
+        },
+        allAuthors: async () => Author.find({})
     },
-    allAuthors: async() => await Author.find({})
-  },
-  Author: {
-    bookCount: async(root) => await Book.countDocuments({author:root._id})
-  },
-  Mutation: {
-    addBook: async (root, args) => {
-      let author = await Author.findOne({ name: args.author })
-
-      if (!author) {
-        author = new Author({ name: args.author })
-        await author.save()
-      }
-
-      const book = new Book({ ...args, author: author._id })
-      return book.save()
+    Author: {
+        bookCount: async (root) => {
+            return Book.countDocuments({ author: root._id })
+        }
     },
+    Mutation: {
+        addBook: async (root, args) => {
+            let author = await Author.findOne({ name: args.author })
 
-    editAuthor: (root, args) => {
-      let author = Author.find(a => a.name === args.name)
-      if (!author) {
-        return null
-      }
-      author.born = args.setBornTo
-      return author
+            if (!author) {
+                author = new Author({ name: args.author })
+                await author.save()
+            }
+
+            const book = new Book({ ...args, author: author._id })
+            return book.save()
+        },
+
+        editAuthor: async (root, args) => {
+            let author = await Author.findOne({ name: args.name })
+
+            if (!author) {
+                return null
+            }
+
+            author.born = args.setBornTo
+            return author.save()
+        }
     }
-  }
 }
 
-module.exports= resolvers
+module.exports = resolvers
